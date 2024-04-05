20a720f54f58d3710a0af12fb16ed6bed3126ebb
abhishek choudhary <abhishek.choudharry@hotmail.com>
2024-04-05 19:54:33 +0530

    Seperated the vnet as a seperate resource

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -51,19 +51,46 @@ resource "azurerm_virtual_network" "vnetterraform" {
   resource_group_name = azurerm_resource_group.rgterraform.name
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.nsg-terraform.name
-  }
+  
 
   tags = local.tag_terraform
 
 }
+resource "azurerm_subnet" "subnet1terraform" {
+  name = "Subnet1"
+  address_prefixes = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnetterraform.name
+  resource_group_name = azurerm_resource_group.rgterraform.name
+  
+}
 
+resource "azurerm_subnet" "subnet2terraform" {
+  name = "Subnet2"
+  address_prefixes = ["10.0.2.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnetterraform.name
+  resource_group_name = azurerm_resource_group.rgterraform.name
+  
+  
+}
 
+resource "azurerm_public_ip" "ipterraform" {
+  name = "ip-terraform"
+  resource_group_name = azurerm_resource_group.rgterraform.name
+  location = azurerm_resource_group.rgterraform.location
+  allocation_method = "Static"
+  tags = local.tag_terraform
+  
+}
+
+resource "azurerm_network_interface" "nicterraform" {
+  name = "nic-terraform"
+  resource_group_name = azurerm_resource_group.rgterraform.name
+  location = azurerm_resource_group.rgterraform.location
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.subnet1terraform.id
+    private_ip_address_allocation = "Dynamic"
+  }
+  tags = local.tag_terraform
+  
+}
