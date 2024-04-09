562aea4e5c47bddcb1d81a8a8cdb121c6eb45946
abhishek choudhary <abhishek.choudharry@hotmail.com>
2024-04-09 21:47:53 +0530

    Added the Usecase for Random Provider

--- Random/main.tf
+++ Random/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.6.0"
     }
   }
@@ -11,3 +11,59 @@ provider "random" {
   # Configuration options
 }
 
+
+resource "random_string" "randomName" {
+  length = 6
+
+}
+
+resource "random_integer" "randomNumber" {
+
+  min = 9999
+  max = 999999
+}
+
+resource "random_password" "randomPassword" {
+  length = 9
+}
+
+resource "random_pet" "randomPetName" {
+
+}
+
+locals {
+  current_timestamp = timestamp()
+  current_day       = formatdate("YYYY-MM-DD", local.current_timestamp)
+  current_time      = formatdate("hh:mm:ss", local.current_timestamp)
+  current_day_name  = formatdate("EEEE", local.current_timestamp)
+}
+
+output "randomName" {
+  value = random_string.randomName.result
+}
+
+output "randomNumber" {
+  value = random_integer.randomNumber.result
+}
+
+output "randomPassword" {
+  value     = random_password.randomPassword
+  sensitive = true
+}
+
+output "randomPetName" {
+  value = random_pet.randomPetName.id
+}
+
+output "current_timestamp" {
+  value = local.current_timestamp
+}
+output "current_day" {
+  value = local.current_day
+}
+output "current_time" {
+  value = local.current_time
+}
+output "current_day_name" {
+  value = local.current_day_name
+}
