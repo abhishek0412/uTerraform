1f89b2bdd8ad86cca6dce64c588af44313be16f8
abhishek choudhary <abhishek.choudharry@hotmail.com>
2024-04-15 22:27:23 +0530

    Added the time provider

--- Random/main.tf
+++ Random/main.tf
@@ -9,6 +9,11 @@ terraform {
       source = "hashicorp/null"
       version = "3.2.2"
     }
+    
+    time = {
+      source = "hashicorp/time"
+      version = "0.11.1"
+    }
   }
 
   backend "azurerm" {
