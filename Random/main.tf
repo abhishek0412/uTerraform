8b66055d2fdbf355c2918dbba72df905e1699c7e
abhishek choudhary <abhishek.choudharry@hotmail.com>
2024-04-15 22:24:28 +0530

    Added the null provider

--- Random/main.tf
+++ Random/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/random"
       version = "3.6.0"
     }
+
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.2"
+    }
   }
 
   backend "azurerm" {
